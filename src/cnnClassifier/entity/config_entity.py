e6467dc0ed87b5283413aaa66c760a2fb6005c88
Vishwajit <vishwajitsarakpatil@email.com>
2025-04-05 14:23:10 +0530

    data ingestion added

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,12 @@
+##entity defining the return type
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+   
